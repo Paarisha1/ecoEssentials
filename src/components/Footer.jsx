415f41645dccf9f40d4c47e1d6ec4d2577ec6b37
Paarisha1 <scin.paarishakaushik27@gmail.com>
2024-01-10 19:28:07 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Footer=()=>{
+    return(
+        <div>
+        <h4  style={{textAlign:"center", marginY:"1rem"}}>
+        © Copyrights 2023 by Paarisha<br/>All Rights Reserved.
+        </h4>
+        </div>
+    )
+}
+export default Footer;
